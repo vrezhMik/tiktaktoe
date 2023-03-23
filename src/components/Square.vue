8b9c5db8b3de3cf697efe512db03571aa2315937
Vrezh Mikayelyan <miqaelyan.vrej1999@gmail.com>
2023-03-23 23:12:34 +0400

    add X and O

--- src/components/Square.vue
+++ src/components/Square.vue
@@ -1,13 +1,37 @@
 <template>
-  <div class="square"></div>
+  <div
+    class="square"
+    :class="{
+      x: Store.state.blocks[props.id] == 'X',
+      o: Store.state.blocks[props.id] == 'O',
+    }"
+  >
+    {{ Store.state.blocks[props.id] }}
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Store from "./../store/index";
+const props = defineProps({
+  id: Number,
+});
+</script>
 <style scoped>
 .square {
   height: 150px;
   width: 150px;
   border: 1px solid black;
   box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 100px;
+}
+.x {
+  color: red;
+}
+
+.o {
+  color: green;
 }
 </style>
