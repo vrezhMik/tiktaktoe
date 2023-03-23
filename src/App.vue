0de6ce3a7a2f6f8e91c5b38a99827e5bdd321513
Vrezh Mikayelyan <miqaelyan.vrej1999@gmail.com>
2023-03-23 20:15:24 +0400

    add user sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <main>
+    <Player />
     <div class="container">
       <Square v-for="i in 9" :key="i" @click="addItem()" />
     </div>
+    <Player />
   </main>
 </template>
 
 <script lang="ts" setup>
 import Square from "./components/Square.vue";
+import Player from "./components/Player.vue";
 
 function addItem(): void {
   console.log("addItem");
@@ -19,8 +22,9 @@ main {
   width: 100%;
   height: 100vh;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+
   .container {
     border: 1px solid black;
     width: 450px;
---
+++ src/components/Player.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="player">
+    <h1>Player:</h1>
+    <h1>Won:</h1>
+    <h1>Loose:</h1>
+  </div>
+</template>
+<script lang="ts" setup></script>
+<style lang="scss" scoped>
+.player {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 150px;
+  row-gap: 25px;
+  height: 100%;
+  width: 250px;
+  background-color: #9ad4ff;
+}
+</style>
